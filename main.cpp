1a64af2cbd01f7aeb2b72fbe041922bbb3a9d342
AnastasiyaVazhenina <32188554+AnastasiyaVazhenina@users.noreply.github.com>
2019-12-14 02:40:27 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,258 +1,242 @@
-#include <algorithm>
 #include <iostream>
-#include <iterator>
+#include <cstdio>
+#include <cstdlib>
 #include <vector>
-#include <thread>
-#include <chrono>
+#include <exception>
+#include <string>
 
-template <class T, class F, class ... Args>
-long timePerform(F&& func, Args&&... args)
-{
-    using std::chrono::high_resolution_clock;
-    auto startTime = std::chrono::high_resolution_clock::now();
+#define BITS_IN_REGISTER 16
+#define TOP_VALUE (((long) 1 << BITS_IN_REGISTER) - 1)
+#define FIRST_QTR (TOP_VALUE / 4 + 1)
+#define HALF (2 * FIRST_QTR)
+#define NO_OF_CHARS 256
+#define EOF_SYMBOL (NO_OF_CHARS + 1)
+#define NO_OF_SYMBOLS (NO_OF_CHARS + 1)
+#define MAX_FREQUENCY 16383
 
-    func(std::forward<Args>(args)...);
+class Arith_adapt {
 
-    auto endTime = std::chrono::high_resolution_clock::now();
+    unsigned char index_to_char[NO_OF_SYMBOLS];
 
-    return std::chrono::duration_cast<T>(endTime - startTime).count();
-}
+    int char_to_index[NO_OF_CHARS];
 
+    int cum_freq[NO_OF_SYMBOLS + 1];
 
-template <class T>
-struct Less
-{
-    bool operator()(T a, T b) const
-    {
-        return a < b;
+    int freq[NO_OF_SYMBOLS + 1] = {0,
+                                   1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 124, 1, 1, 1, 1, 1,
+                                   1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                                   1236, 1, 21, 9, 3, 1, 25, 15, 2, 2, 2, 1, 79, 19, 60, 1,
+                                   15, 15, 8, 5, 4, 7, 5, 4, 4, 6, 3, 2, 1, 1, 1, 1,
+                                   1, 24, 15, 22, 12, 15, 10, 9, 16, 16, 8, 6, 12, 23, 13, 11,
+                                   14, 1, 14, 28, 29, 6, 3, 11, 1, 3, 1, 1, 1, 1, 1, 3,
+                                   1, 491, 85, 173, 232, 744, 127, 110, 293, 418, 6, 39, 250, 139, 429, 446,
+                                   111, 5, 388, 375, 531, 152, 57, 97, 12, 101, 5, 2, 1, 2, 3, 1,
+                                   1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                                   1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                                   1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                                   1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                                   1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                                   1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                                   1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                                   1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                                   1};
+
+    long low;
+    long high;
+    long value;
+
+    long bits_to_follow;
+    int buffer;
+    int bits_to_go;
+    int garbage_bits;
+
+    FILE *in{};
+    FILE *out{};;
+
+    void start_model() {
+        for (int i = 0; i < NO_OF_CHARS; ++i) {
+            char_to_index[i] = i + 1;
+            index_to_char[i + 1] = i;
+        }
+        cum_freq[NO_OF_SYMBOLS] = 0;
+        for (int i = NO_OF_SYMBOLS; i > 0; --i) {
+            cum_freq[i - 1] = cum_freq[i] + freq[i];
+        }
+        if (cum_freq[0] > MAX_FREQUENCY)
+            std::exit(1);
     }
+
+    void start_inputing_bits() {
+        bits_to_go = 0;
+        garbage_bits = 0;
+    }
+
+    int input_bit() {
+        if (bits_to_go == 0) {
+            buffer = std::getc(in);
+            if (buffer == EOF) {
+                garbage_bits += 1;
+                if (garbage_bits > (BITS_IN_REGISTER - 2)) {
+                    throw std::runtime_error("Error in file");
+                }
+            }
+            bits_to_go = 8;
+        }
+        int t = buffer & 1;
+        buffer >>= 1;
+        bits_to_go -= 1;
+        return t;
+    }
+
+    void start_outputing_bits() {
+        buffer = 0;
+        bits_to_go = 8;
+    }
+
+
+    void output_bit(int bit) {
+        buffer >>= 1;
+        if (bit)
+            buffer |= 0x80;
+        bits_to_go -= 1;
+        if (bits_to_go == 0) {
+            std::putc(buffer, out);
+            bits_to_go = 8;
+        }
+    }
+
+    void done_outputing_bits() {
+        std::putc(buffer >> bits_to_go, out);
+    }
+
+    void output_bit_plus_follow(int bit) {
+        output_bit(bit);
+        while (bits_to_follow > 0) {
+            bit = !bit;
+            output_bit(bit);
+            --bits_to_follow;
+        }
+    }
+
+    void start_encoding() {
+        low = 0;
+        high = TOP_VALUE;
+        bits_to_follow = 0;
+    }
+
+    void done_encoding() {
+        ++bits_to_follow;
+        if (low < FIRST_QTR)
+            output_bit_plus_follow(0);
+        else
+            output_bit_plus_follow(1);
+    }
+
+    void start_decoding() {
+        for (int i = 0; i < BITS_IN_REGISTER; ++i) {
+            value = 2 * value + input_bit();
+        }
+        low = 0;
+        high = TOP_VALUE;
+    }
+
+    void encode_symbol(int symbol) {
+        long range = (high - low) + 1;
+        high = low + (range * cum_freq[symbol - 1]) / cum_freq[0] - 1;
+        low = low + (range * cum_freq[symbol]) / cum_freq[0];
+        for (;;) {
+            if (high < HALF) {
+                output_bit_plus_follow(0);
+                low = 2 * low;
+                high = 2 * high + 1;
+            } else if (low >= HALF) {
+                output_bit_plus_follow(1);
+                low = 2 * (low - HALF);
+                high = 2 * (high - HALF) + 1;
+            } else
+                break;
+        }
+    }
+
+    int decode_symbol() {
+        int symbol = 0;
+        long range = (high - low) + 1;
+        int cum = (((value - low) + 1) * cum_freq[0] - 1) / range;
+        for (symbol = 1; cum_freq[symbol] > cum; ++symbol);
+        high = low + (range * cum_freq[symbol - 1]) / cum_freq[0] - 1;
+        low = low + (range * cum_freq[symbol]) / cum_freq[0];
+        for (;;) {
+            if (high < HALF) {
+                value = 2 * value + input_bit();
+                low = 2 * low;
+                high = 2 * high + 1;
+            } else if (low > HALF) {
+                value = 2 * (value - HALF) + input_bit();
+                low = 2 * (low - HALF);
+                high = 2 * (high - HALF) + 1;
+            } else
+                break;
+        }
+        return symbol;
+    }
+
+public:
+    void encode(const char *input, const char *output) {
+        in = std::fopen(input, "r+b");
+        out = std::fopen(output, "w+b");
+        if (in == nullptr || out == nullptr)
+            return;
+        start_model();
+        start_outputing_bits();
+        start_encoding();
+        for (;;) {
+            int ch = std::getc(in);
+            if (std::feof(in))
+                break;
+            int symbol = char_to_index[ch];
+            encode_symbol(symbol);
+        }
+        int eof = EOF_SYMBOL;
+        encode_symbol(eof);
+        done_encoding();
+        done_outputing_bits();
+        std::fclose(in);
+        std::fclose(out);
+    }
+
+    void decode(const char *input, const char *output) {
+        in = std::fopen(input, "r+b");
+        out = std::fopen(output, "w+b");
+        if (in == nullptr || out == nullptr)
+            return;
+        start_model();
+        start_inputing_bits();
+        start_decoding();
+        for (;;) {
+            int symbol = decode_symbol();
+            if (symbol == EOF_SYMBOL)
+                break;
+            int ch = index_to_char[symbol];
+            std::putc(ch, out);
+        }
+        std::fclose(in);
+        std::fclose(out);
+    }
+
+    Arith_adapt() : low(0), high(0), value(0), bits_to_follow(0), buffer(0), bits_to_go(0), garbage_bits(0) {}
 };
 
-template <class It, class Out, class Compare>
-Out merge(It first1, It last1, It first2, It last2, Out out, Compare cmp)
-{
-    int k = 0;
-    while ((first1 != last1) && (first2 != last2))
+int main(int argc, char* argv[]) {
+    if (argc < 4)
     {
-        if (cmp(*first1, *first2))
-        {
-            out[k] = *first1;
-            ++k;
-            ++first1;
-        }
-        else
-        {
-            out[k] = *first2;
-            ++k;
-            ++first2;
-        }
+        std::cout << "\n Using: Arith_adapt infile outfile outfile_decode\n";
+        return 0;
     }
-    while (first1 < last1)
-    {
-        out[k] = *first1;
-        ++k;
-        ++first1;
-    }
-    while (first2 < last2)
-    {
-        out[k] = *first2;
-        ++k;
-        ++first2;
-    }
-    return out;
-}
-
-template <class It, class Out, class Compare>
-Out merge_sort(It first, It last, Out out, Compare cmp)
-{
-    size_t size = last - first;
-    if (cmp(1, size))
-    {
-        size_t middle = size / 2;
-        out = merge_sort<It, Out, Compare>(first, first + middle, out, cmp);
-        out = merge_sort<It, Out, Compare>(first + middle, last, out, cmp);
-        out = merge<It, Out, Compare>(first, first + middle, first + middle, last, out, cmp);
-        It curr = first;
-        size_t j = 0;
-        while (curr != last)
-        {
-            *curr = out[j];
-            ++curr;
-            ++j;
-        }
-    }
-    return out;
-}
-
-template <class It, class Compare>
-void inplace_merge_sort(It first, It last, Compare cmp)
-{
-    size_t size = last - first;
-    if (size < 2)
-    {
-        return;
-    }
-    size_t middle = size / 2;
-    inplace_merge_sort(first, first + middle, cmp);
-    inplace_merge_sort(first + middle, last, cmp);
-    std::inplace_merge(first, first + middle, last, cmp);
-}
-
-template <class It, class Compare>
-void maxHeapify(It first, It last, size_t index, Compare cmp)
-{
-    size_t size = last - first;
-    size_t largest = index;
-    size_t left = 2 * index;
-    size_t right = 2 * index + 1;
-    if (!cmp(size, left))
-    {
-        if (cmp(*(first + index - 1), *(first + left - 1)))
-        {
-            largest = left;
-        }
-    }
-    if (!cmp(size, right))
-    {
-        if (cmp(*(first + largest - 1), *(first + right - 1)))
-        {
-            largest = right;
-        }
-    }
-    if (largest != index)
-    {
-        std::iter_swap(first + index - 1, first + largest - 1);
-        maxHeapify<It, Compare>(first, last, largest, cmp);
-    }
-}
-template <class It, class Compare>
-void heap_sort(It first, It last, Compare cmp)
-{
-    size_t size = last - first;
-    for (size_t i = size / 2; i > 0; --i)
-    {
-        maxHeapify<It, Compare>(first, last, i, cmp);
-    }
-    int tmp = 0;
-    for (size_t i = size; i > 1; --i)
-    {
-        std::iter_swap(first, first + i - 1);
-        ++tmp;
-        It iter = last - tmp;
-        maxHeapify<It, Compare>(first, iter, 1, cmp);
-    }
-}
-
-template <class It, class Compare>
-void quick_sort(It first, It last, Compare cmp)
-{
-    if (last != first)
-    {
-        It i = first;
-        for (It j = first; j != last; ++j)
-        {
-            if (cmp(*j, *(last - 1)))
-            {
-                std::swap(*i, *j);
-                ++i;
-            }
-        }
-        std::iter_swap(i, last - 1);
-        quick_sort(first, i, cmp);
-        quick_sort(i + 1, last, cmp);
-    }
-}
-
-template <class It, class Compare>
-void insertion_sort(It first, It last, Compare cmp)
-{
-    for (It curr = first + 1; curr != last; ++curr)
-    {
-        bool tmp = false;
-        auto temp = *curr;
-        It item = curr - 1;
-        while (cmp(temp, *item))
-        {
-            *(item + 1) = *item;
-            if (item == first)
-            {
-                tmp = true;
-                break;
-            }
-            --item;
-        }
-        if (tmp)
-        {
-            *item = temp;
-        }
-        else
-        {
-            *(item + 1) = temp;
-        }
-    }
-}
-
-int main()
-{
-    std::vector<int> vec1 = {0, 4, 6, 8, 9};
-    std::vector<int> vec2 = {1, 2, 5, 7};
-    std::vector<int> out;
-    out.resize(vec1.size() + vec2.size());
-    Less<int> cmp;
-    out = merge(vec1.begin(), vec1.end(), vec2.begin(), vec2.end(), out, cmp);
-    std::vector<int> set = {7, 1, 8, 2, 9, 3, 4, 5, 6, 0};
-    set = merge_sort(set.begin(), set.end(), set, cmp);
-    std::vector<int> vector = {7, 1, 8, 2, 9, 3, 4, 5, 6, 0};
-    inplace_merge_sort(vector.begin(), vector.end(), cmp);
-    std::vector<int> vec = {7, 1, 8, 2, 9, 3, 4, 5, 6, 0};
-    heap_sort(vec.begin(), vec.end(), cmp);
-    std::vector<int> set1 = {7, 1, 8, 2, 9, 3, 4, 5, 6, 0};
-    quick_sort(set1.begin(), set1.end(), cmp);
-    std::vector<int> set2 = {7, 1, 8, 2, 9, 3, 4, 5, 6, 0};
-    insertion_sort(set2.begin(), set2.end(), cmp);
-    long time1 = 0;
-    long time2 = 0;
-    long time3 = 0;
-    long time4 = 0;
-    long time5 = 0;
-    long time6 = 0;
-    long timer1 = 0;
-    long timer2 = 0;
-    long timer3 = 0;
-    long timer4 = 0;
-    long timer5 = 0;
-    long timer6 = 0;
-    for(int j = 0; j < 30; ++j)
-    {
-        timer1 = timePerform<std::chrono::microseconds>(merge, vec1.begin(), vec1.end(),
-                                                        vec2.begin(), vec2.end(), out, cmp);
-        time1 += timer1;
-        std::cout << timer1 << ' ';
-        timer2 = timePerform<std::chrono::microseconds>(merge_sort, set.begin(), set.end(), set, cmp);
-        time2 += timer2;
-        std::cout << timer2 << ' ';
-        timer3 = timePerform<std::chrono::microseconds>(inplace_merge_sort, vector.begin(), vector.end(), cmp);
-        time3 += timer3;
-        std::cout << timer3 << ' ';
-        timer4 = timePerform<std::chrono::microseconds>(heap_sort, vec.begin(), vec.end(), cmp);
-        time4 += timer4;
-        std::cout << timer4 << ' ';
-        timer5 = timePerform<std::chrono::microseconds>(quick_sort, set1.begin(), set1.end(), cmp);
-        time5 += timer5;
-        std::cout << timer5 << ' ';
-        timer6 = timePerform<std::chrono::microseconds>(insertion_sort, set2.begin(), set2.end(), cmp);
-        time6 += timer6;
-        std::cout << timer6 << ' ';
-        std::cout << std::endl;
-    }
-    std::cout << std::endl;
-    std::cout << time1 / 30 << ' ';
-    std::cout << time2 / 30 << ' ';
-    std::cout << time3 / 30 << ' ';
-    std::cout << time4 / 30 << ' ';
-    std::cout << time5 / 30 << ' ';
-    std::cout << time6/ 30 << ' ';
-    std::cout << std::endl << std::endl;
-
+    std::string input = argv[1];
+    std::string output = argv[2];
+    std::string output_decode = argv[3];
+    Arith_adapt model;
+    model.encode(input.c_str(), output.c_str());
+    model.decode(output.c_str(), output_decode.c_str());
     return 0;
 }
